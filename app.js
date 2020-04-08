60eb003aab26fad221ed2d0d5e5b84381a595a2e
bbtwots <brettbelka@gmail.com>
2020-04-07 21:20:02 -0700

    Some validation. Cleaned up

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,21 +11,38 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const confirmString = async (input) => {
+    if (input === "") {
+        return 'Please provide an answer.';
+    }
+    return true;
+};
+
+const confirmNumber = async (input) => {
+    if (isNaN(input)) {
+        return 'Please provide a numerical id number.';
+    }
+    return true;
+};
+
 const questions = [
     {
         type: "input",
         name: "name",
-        message: "What is the employee's name?"
+        message: "What is the employee's name?",
+        validate: confirmString
     },
     {
         type: "number",
         name: "id",
-        message: "What is the employee's id?"
+        message: "What is the employee's id number?",
+        validate: confirmNumber
     },
     {
         type: "input",
         name: "email",
-        message: "What is the employee's email?"
+        message: "What is the employee's email?",
+        validate: confirmString
     },
     {
         type: "list",
@@ -40,24 +58,27 @@ const questions = [
         type: "input",
         name: "school",
         message: "What shool does the Intern go to?",
+        validate: confirmString,
         when: (answers) => answers.title === "Intern"
     },
     {
         type: "input",
         name: "github",
         message: "What is the Engineer's github username?",
+        validate: confirmString,
         when: (answers) => answers.title === "Engineer"
     },
     {
         type: "input",
         name: "officeNumber",
         message: "What is the Manager's office number?",
+        validate: confirmString,
         when: (answers) => answers.title === "Manager"
     },
     {
         type: "confirm",
         name: "keepGoing",
-        message: "Would you like to keep entering employees?"
+        message: "Would you like to keep entering employees?",
     }
 ]
 
@@ -80,26 +101,16 @@ const write = () => {
     empData.forEach(employee => {
         let newEmp
         if (employee.title === "Manager") {
-            let name = employee.name;
-            let id = employee.id;
-            let email = employee.email;
-            let officeNumber = employee.officeNumber;
-            newEmp = new Manager(name, id, email, officeNumber);
+            newEmp = new Manager(employee.name, employee.id, employee.email, employee.officeNumber);
+
         } else if (employee.title === "Intern") {
-            let name = employee.name;
-            let id = employee.id;
-            let email = employee.email;
-            let school = employee.school;
-            newEmp = new Intern(name, id, email, school);
+            newEmp = new Intern(employee.name, employee.id, employee.email, employee.school);
+
         } else if (employee.title === "Engineer") {
-            let name = employee.name;
-            let id = employee.id;
-            let email = employee.email;
-            let github = employee.github;
-            newEmp = new Engineer(name, id, email, github);
+            newEmp = new Engineer(employee.name, employee.id, employee.email, employee.github);
         };
+
         employees.push(newEmp);
-        
 
     })
     console.log(employees);
@@ -109,16 +120,11 @@ const write = () => {
 const writeHTML = (employees) => {
     const renderedHTML = render(employees);
     console.log(renderedHTML);
-
     fs.writeFile(outputPath, renderedHTML, function (err) {
-
         if (err) {
             return console.log(err);
         }
-
         console.log("Successfully written!");
-
     })
 }
-
 ask();
